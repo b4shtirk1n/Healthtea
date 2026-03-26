f49f39f75be996bf9fb79602490cb0971317907c
b4shtirk1n <wowan4512@gmail.com>
2026-03-26 03:55:12 +0300

    UPDATE: raise provider

--- providers/telegramProvider.py
+++ providers/telegramProvider.py
@@ -2,13 +2,15 @@ import os
 from telegram.ext import Application
 from abstractions.baseProvider import BaseProvider
 
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 TG_USER_ID = os.getenv("TG_USER_ID")
 
+if not BOT_TOKEN or not TG_USER_ID:
+    raise
+
 
 class TelegramProvider(BaseProvider):
-    application = Application.builder().token(BOT_TOKEN).build()
+    application = Application.builder().token(BOT_TOKEN).build()  # type: ignore
 
     async def send_report(self, msg: str):
         await self.application.bot.send_message(TG_USER_ID, msg)
